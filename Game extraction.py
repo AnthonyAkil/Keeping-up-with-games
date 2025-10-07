3d83d19553cb2de9cd879d3a5b49efea48762b05
Anthony Akil <anthonyakil@hotmail.nl>
2025-10-07 22:18:18 +0200

    Pivoting data sources: Steam -> IGDB API.

--- Game extraction.py
+++ Game extraction.py
@@ -1,122 +1,132 @@
+"""
+Internet Games Data Base (IGDB) API Data Extraction script
+Extracts game data from the IGDB API and prepares files for DWH ingestion.
+"""
+
+# ============================================================================
+# IMPORTS
+# ============================================================================
+
 import requests
-import json
+import configparser
+from math import ceil
 import time
 import polars as pl
-from datetime import date
-import os
-
-
-def get_all_app_ids():
-    url         = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-    response    = requests.get(url)
-    response.raise_for_status()
-    all_apps    = response.json()["applist"]["apps"]
-    return [all_apps[app]["appid"] for app in range(len(all_apps))]
-
-
-def get_app_details(appid: int, country_code: str = "nl", language: str = "english"):
-    """
-    Input:
-    - appid (int):            The id of the app/game.
-    - country_code (str):     Two-letter country code (ISO 3166-1 alpha-2), for respective currency and regional availability.
-    - language (str):         Language of the text fields.
-
-    Returns:
-    - Information regarding the input appid, in JSON format.
-
-    """
-    url         = "https://store.steampowered.com/api/appdetails"
-    params      = {"appids": appid, "cc": country_code, "l": language}
-    response    = requests.get(url, params=params)
-    response.raise_for_status()
-    app_data    = response.json()
-    app_key     = str(appid)
-    if (app_key in app_data) and (app_data[app_key]["success"]):  # if the id was actually called and a success, return the data
-        return app_data[app_key]["data"]
-    return None
-
-
-# Store parsed game data in one big dictionary
-all_game_data           = {}
-
-# Store category and genre data in a list of dictionaries
-all_game_categories     = []
-game_category_tag       = []
-all_game_genres         = []
-game_genre_tag          = []
-
-# Used to avoid storing duplicate categories and genres 
-unique_game_categories  = set()
-unique_game_genres      = set()
-
-
-# Extracting data:
-all_app_ids = get_all_app_ids()
-
-for app_id in all_app_ids:
-    app_details = get_app_details(app_id)
-    if app_details:
-        all_game_data[app_id] = {
-            "name":             app_details.get("name"),
-            "type":             app_details.get("type"),
-            "release_date":     app_details.get("release_date", {}).get("date"),  # HACK: handles games with no release date
-            "is_free":          app_details.get("is_free"),
-            "price_overview":   app_details.get("price_overview"),
-            "metacritic_score": app_details.get("metacritic", {}).get("score"),
-        }
-        
-        for category in app_details.get("categories", []):
-            category_id, category_name = category["id"], category["description"]
-            if category_id not in unique_game_categories:
-                all_game_categories.append({
-                    "category_id":      category_id, 
-                    "category_name":    category_name
-                })
-                unique_game_categories.add(category_id)
-
-            game_category_tag.append({
-                "game_id":      app_id,
-                "category_id":  category_id,
-            })
-
-        for genre in app_details.get("genres", []):
-            genre_id, genre_name = genre["id"], genre["description"]
-            if genre_id not in unique_game_genres:
-                all_game_genres.append({
-                    "genre_id":     genre_id, 
-                    "genre_name":   genre_name
-                })
-                unique_game_genres.add(genre_id)
-
-            game_genre_tag.append({
-                "game_id":  app_id,
-                "genre_id": genre_id,
-            })
-
-    time.sleep(1)
 
 
 
-# TODO: write to a better file structure for easier Load process
+# ============================================================================
+# Script Configuration
+# ============================================================================
+api_rate_limit : int        = 500
+rate_limit_delay : float    = 0.25     # 1 request / 0.25s
+multiquery_size_limit :int  = 10
 
-# Writing the data to Parquet files:
-date_today = date.today().isoformat
 
-data_tables = {
-    "games":                all_game_data,
-    "categories":           all_game_categories,
-    "game_category_tag":    game_category_tag,
-    "genres":               all_game_genres,
-    "game_genre_tag":       game_genre_tag,
+auth_url : str    = "https://id.twitch.tv/oauth2/token"
+url      : str    = "https://api.igdb.com/v4/"
+
+data_field_names : str = "id, name, first_release_date, game_modes, game_type, game_status, genres, platforms, total_rating, total_rating_count, dlcs, franchise, hypes"
+
+
+parser = configparser.ConfigParser()
+parser.read("credentials.conf")
+client_id = parser.get(
+    "igdb_credentials",
+    "client_id"
+)
+client_secret = parser.get(
+    "igdb_credentials",
+    "client_secret"
+)
+auth_params = {
+    "client_id":        client_id,
+    "client_secret":    client_secret,
+    "grant_type":       "client_credentials"
+} 
+
+
+local_output_file : str = "igdb_api_data_test.parquet"
+  
+
+# ============================================================================
+# Calling api access token
+# ============================================================================
+
+response = requests.post(auth_url, params = auth_params)
+access_token = response.json()["access_token"]
+headers     = {
+    "Client-ID":        client_id,
+    "Authorization":    f"Bearer {access_token}",
+    "Accept":           "application/json"
 }
 
-for name, records in data_tables.items():
-    # Move on to the next table if there are no records
-    if not records:
+
+# ============================================================================
+# Function setup
+# ============================================================================
+
+
+def get_total_games_count(base_url: str, input_headers : dict) -> int:
+    
+    response = requests.post(base_url + "games/count", headers = input_headers)
+    response.raise_for_status()
+    return response.json()['count']
+
+def create_offset_batches(offset_list: list[int], step_size: int):
+    """
+    Create a list of lists, where each entry corresponds to the offset ranges required to execute 1 multiquery request.
+    Yield allows for a single call and one-time calculation.
+    """
+    for start_point in range(0, len(offset_list), step_size):
+        yield offset_list[start_point : start_point + step_size]
+
+
+# ============================================================================
+# Calling data endpoints
+# ============================================================================
+
+total_games_count   = get_total_games_count(base_url = url, input_headers= headers)
+total_pages         = ceil(total_games_count / api_rate_limit)
+offsets             = [page_number * api_rate_limit for page_number in range(total_pages)]
+offset_batches      = list(create_offset_batches(offsets, multiquery_size_limit))
+num_api_calls       = len(offset_batches)
+
+dataframes = []
+
+for batch_index, offset_batch in enumerate(offset_batches, start = 1):
+    
+    multiquery = ""
+    for subquery_index, offset in enumerate(offset_batch):
+        multiquery += f"""
+        query games "page_{batch_index}_{subquery_index}" {{
+        fields {data_field_names};
+        limit {api_rate_limit};
+        offset {offset};
+    }};
+    """
+    
+    try:
+        response = requests.post(url + "multiquery", headers = headers, data = multiquery)
+        response.raise_for_status()
+        batch_results = []
+        data = response.json()
+        
+        for subquery in range(len(data)):
+            batch_results.extend(data[subquery]["result"])
+            
+        if batch_results:
+            dataframes.append(pl.DataFrame(batch_results))
+        else:
+            print(f"Empty batch on {batch_index}/{num_api_calls}")
+        time.sleep(rate_limit_delay)
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error on batch {batch_index}: {e}")
+        print("Attempting to sleep 5 seconds before next call...")
+        time.sleep(5)
         continue
-    folder = f"data/{name}/extraction_date = {today}"
-    os.makedirs(folder, exist_ok=True)
-    pl.DataFrame(records).write_parquet(f"{folder}/{name}.parquet")
-
-
-# TODO: decide on update frequency and implement upsert strategy when needed
+        
+ 
+merged_dataframe = pl.concat(dataframes, how = "vertical")
+merged_dataframe.write_parquet(local_output_file, compression = "zstd", compression_level = 3) # zstd:3 is a good tradeoff between compression-read for analytical ELT
