7d22d9289f51cfc42f3ee5408807dded911c62d1
Anthony Akil <anthonyakil@hotmail.nl>
2026-04-15 22:18:51 +0200

    Data cleaning and NULL handling.

--- dbt_transformation_layer/models/silver/CLEAN_GAMES.sql
+++ dbt_transformation_layer/models/silver/CLEAN_GAMES.sql
@@ -2,9 +2,17 @@ SELECT
     id                 AS "Game ID",
     name               AS "Game name",
     game_type          AS "Game type ID",
-    total_rating       AS "Rating average",
-    total_rating_count AS "Rating count",
-    hypes              AS "Wishlist count",
+    total_rating       AS "Average rating",
+    CASE
+        WHEN total_rating_count = 0 
+        AND total_rating > 0 AND total_rating < 95 -- A rating higher than 95 proved to have an actual review
+            THEN 1
+        WHEN total_rating_count > 0
+        AND total_rating = 0
+            THEN 0
+        ELSE ZEROIFNULL(total_rating_count)        -- NULL reviews is synonymous to no reviews
+    END                AS "Number of reviews",
+    ZEROIFNULL(hypes)  AS "Wishlist count",        -- NULL wishlists is synonymous to no wishlists
     TO_TIMESTAMP(
         first_release_date
     )::DATE     AS "Date game initial release"     -- Converting the UNIX timestamp to GMT date
