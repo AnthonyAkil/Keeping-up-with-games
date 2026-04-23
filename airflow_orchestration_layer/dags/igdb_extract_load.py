4440cd2edb80427a6f726b6af4ecb89613947567
Anthony Akil <anthonyakil@hotmail.nl>
2026-04-23 22:24:17 +0200

    Adding a DAG to trigger the COPY INTO from the external stage.

--- airflow_orchestration_layer/dags/igdb_extract_load.py
+++ airflow_orchestration_layer/dags/igdb_extract_load.py
@@ -1,9 +1,17 @@
-from airflow.sdk import dag, task
+from airflow.sdk import dag, task, get_current_context
 from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
 
+ENDPOINT_TABLE_MAP = {
+    "games":      "RAW_GAMES",
+    "game_modes": "RAW_GAMEMODE",
+    "game_types": "RAW_GAMETYPE",
+    "genres":     "RAW_GENRE",
+    "platforms":  "RAW_PLATFORM",
+    "franchises": "RAW_FRANCHISE",
+}
 
 @dag(
     dag_id="igdb_pipeline",
@@ -16,22 +24,78 @@ logger = logging.getLogger(__name__)
 def igdb_pipeline():
 
     @task(retries=2, retry_delay=300)
-    def run_igdb_pipeline():
+    def run_igdb_pipeline(run_date: int):
         
         import sys
         sys.path.append("/opt/airflow")
+        
         
         try:
             from api_extract_data.api_to_storage import main as extract_load    # Pushes down the import as this is considered top-level code
 
             logger.info("Extracting and loading IGDB data...")
-            extract_load()
+            extract_load(run_date)
 
         except Exception as e:
             logger.exception("IGDB pipeline failed")
             raise e
 
-    run_igdb_pipeline()
+    @task(retries=2, retry_delay=300)
+    def copy_into_snowflake(run_date: int):
+        from api_extract_data.get_keyvault_secrets import get_secret_client, get_secret
+        import os
+        from dotenv import load_dotenv
+        import snowflake.connector
+        import sys
+        sys.path.append("/opt/airflow")
+        
+        load_dotenv()
+
+        
+        client = get_secret_client(os.environ.get("AZURE_VAULT_URL"))
+
+        conn = snowflake.connector.connect(
+            account=get_secret(client, "snowflake-account"),
+            user=get_secret(client, "snowflake-username"),
+            password=get_secret(client, "snowflake-password"),
+            database=get_secret(client, "snowflake-database"),
+            schema=get_secret(client, "snowflake-schema"),
+            warehouse=get_secret(client, "snowflake-warehouse"),
+        )
+
+        try:
+            cursor = conn.cursor()
+            for endpoint, table in ENDPOINT_TABLE_MAP.items():
+                file_path = f"{run_date}/{endpoint}.parquet"
+                sql = f"""
+                    COPY INTO {table}
+                    FROM @BLOB_PARQUET_STAGE
+                    FILE_FORMAT = (TYPE = PARQUET)
+                    MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
+                    ON_ERROR = CONTINUE
+                    FILES = ('{file_path}');
+                """
+                try:
+                    logger.info(f"Copying {file_path} into {table}...")
+                    cursor.execute(f"TRUNCATE TABLE {table}")
+                    cursor.execute(sql)
+                    logger.info(f"Successfully copied {endpoint} into {table}")
+                except Exception as e:
+                    logger.error(f"Failed COPY INTO for {endpoint}: {e}")
+                    raise
+        finally:
+            cursor.close()
+            conn.close()
+            
+
+    @task
+    def get_run_date():
+        context = get_current_context()
+        return context["ds_nodash"]
+
+    run_date = get_run_date()
+
+    run_igdb_pipeline(run_date) >> copy_into_snowflake(run_date)
 
 
 igdb_pipeline()
